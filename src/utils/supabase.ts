1be55eb8ac1b6af9bbff4e8aa61cdf70d56f4a90
AKM Zawadur Rafid <zawadurrafid7@gmail.com>
2026-08-21 19:25:52 +0600

    Fixed supabase anon key

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 import "react-native-url-polyfill/auto";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
+const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase environment variables in .env.local");
